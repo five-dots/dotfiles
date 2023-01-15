ce1b51803a93f51f52b07371d5e07db607e01583
Shun Asai <shun.asai@10x.co.jp>
2023-01-15 15:46:19 +0900

    Add clipboard setting  for wsl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,23 @@ do
     vim.opt.smartcase = true
 end
 
+-- Clipboard by win32yank
+-- https://stackoverflow.com/questions/68435130/wsl-clipboard-win32yank-in-init-lua
+if vim.fn.has("wsl") then
+    vim.g.clipboard = {
+        name = "win32yank-wsl",
+        copy = {
+            ["+"] = "win32yank.exe -i --crlf",
+            ["*"] = "win32yank.exe -i --crlf"
+        },
+        paste = {
+            ["+"] = "win32yank.exe -o --crlf",
+            ["*"] = "win32yank.exe -o --crlf"
+        },
+        cache_enable = 0,
+    }
+end
+
 -- require("options")
 -- require("plugins")
 
