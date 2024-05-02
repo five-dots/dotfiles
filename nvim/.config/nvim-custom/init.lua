2f4ffad859d2688a08649360839cc762387a8953
Shun Asai <shun.asai@tmkc.co.jp>
2024-05-02 15:14:03 +0900

    Pass just plugins dir to lazy.nvim

--- nvim/.config/nvim-custom/init.lua
+++ nvim/.config/nvim-custom/init.lua
@@ -10,22 +10,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  require("plugins.comment"),
-  require("plugins.guess-indent"),
-  require("plugins.indent-blankline"),
-  require("plugins.lualine"),
-  require("plugins.noice"),
-  require("plugins.nvim-autopairs"),
-  require("plugins.nvim-cmp"),
-  require("plugins.nvim-lspconfig"),
-  require("plugins.nvim-surround"),
-  require("plugins.nvim-tree"),
-  require("plugins.nvim-treesitter"),
-  require("plugins.nvim-ufo"),
-  require("plugins.onedark"),
-  require("plugins.telescope"),
-  require("plugins.toggleterm"),
-  require("plugins.vimdoc-ja"),
-  require("plugins.which-key"),
-})
+require("lazy").setup("plugins")
--- nvim/.config/nvim-custom/lua/plugins/doom-one.lua
+++ nvim/.config/nvim-custom/lua/plugins/doom-one.lua
@@ -1,5 +1,6 @@
 return {
   "NTBBloodbath/doom-one.nvim",
+  enabled = false,
   config = function()
     vim.g.doom_one_italic_comments = true
     vim.cmd("colorscheme doom-one")
