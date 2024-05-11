1b92cb8c05262ffe9ebf47ec4d02a0ccefbb42e7
Shun Asai <shun.asai@tmkc.co.jp>
2024-05-11 11:05:03 +0900

    Disable noice.nvim functions except cmdline

--- nvchad/.config/nvim-nvchad/lua/plugins/init.lua
+++ nvchad/.config/nvim-nvchad/lua/plugins/init.lua
@@ -165,14 +165,19 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-      -- add any options here
+      -- cmdline = { enabled = false },
+      messages = { enabled = false },
+      notify = { enabled = false },
+      popupmenu = { enabled = false },
+      lsp = {
+        progress = { enabled = false },
+        message = { enabled = false },
+        hover = { enabled = false },
+        signature = { enabled = false },
+      },
     },
     dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
     },
   },
