80fc55fd2c284dab7a6df5aff3237b949c6b98a8
Shun Asai <shun.asai@tmkc.co.jp>
2024-05-15 12:52:06 +0900

    Add bqls lsp config

--- nvchad/.config/nvim-nvchad/lua/configs/lspconfig.lua
+++ nvchad/.config/nvim-nvchad/lua/configs/lspconfig.lua
@@ -10,12 +10,39 @@ capabilities.textDocument.foldingRange = {
 }
 
 local lspconfig = require "lspconfig"
+
+-- Add bqls config
+local configs = require "lspconfig.configs"
+if not configs.bqls then
+  configs.bqls = {
+    default_config = {
+      cmd = { "bq-language-server", "--stdio" },
+      filetypes = { "sql" },
+      root_dir = function(fname)
+        return lspconfig.util.find_git_ancestor(fname) or vim.fn.fnamemodify(fname, ':h')
+      end,
+      settings = {
+        bqExtensionVSCode = {
+          diagnostic = {
+            forVSCode = false
+          },
+          formatting = {
+            printKeywordsInUpperCase = false,
+          },
+        },
+      },
+    },
+  }
+end
+-- lspconfig.bqls.setup()
+
 local servers = {
   -- Default servers (lua_ls is set by default, no need to add it)
   "cssls",
   "html",
   -- Additional servers
   "bashls",
+  "bqls",
   "gopls",
   "jsonls",
   "pyright",
