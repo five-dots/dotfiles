a77facb7892a95f1e864a0d66e3bc0059a8f1f80
Shun Asai <shun.asai@tmkc.co.jp>
2024-05-16 15:05:58 +0900

    Change function definition for nvim-tree on_attach

--- nvchad/.config/nvim-nvchad/lua/plugins/init.lua
+++ nvchad/.config/nvim-nvchad/lua/plugins/init.lua
@@ -1,22 +1,5 @@
 local cmp = require "cmp"
 
-local function on_attach_nvimtree(bufnr)
-  local api = require "nvim-tree.api"
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-  -- Default mappings
-  api.config.mappings.default_on_attach(bufnr)
-
-  -- leap.nvim が使えるように s/S を削除する
-  vim.keymap.del("n", "s", { buffer = bufnr }) -- api.node.run.system
-  vim.keymap.del("n", "S", { buffer = bufnr }) -- api.tree.search_node
-
-  -- Run sysmtem は ! に割り当て
-  vim.keymap.set('n', '!', api.node.run.system, opts('Run System'))
-end
-
 return {
   -- conform.nvim
   {
@@ -100,7 +83,31 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     opts = {
-      on_attach = on_attach_nvimtree,
+      on_attach = function(bufnr)
+        local api = require "nvim-tree.api"
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+        -- Default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        --[[ デフォルトの mapping を削除 ]]
+        -- leap.nvim が使えるように s/S を削除する
+        vim.keymap.del("n", "s", { buffer = bufnr }) -- api.node.run.system
+        vim.keymap.del("n", "S", { buffer = bufnr }) -- api.tree.search_node
+        -- Ctrl-V は Paste が動いてしまうため削除
+        vim.keymap.del("n", "<c-v>", { buffer = bufnr }) -- api.node.open.vertical
+        -- Ctrl-X は押しにくいため削除
+        vim.keymap.del("n", "<c-x>", { buffer = bufnr }) -- api.node.open.horizontal
+
+        --[[ 再割り当てする  ]]
+        -- Run sysmtem は ! に割り当て
+        vim.keymap.set('n', '!', api.node.run.system, opts('Run System'))
+        -- 分割して開く系や Preview をより使いやすいキーに mapping
+        vim.keymap.set('n', '<c-h>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+        vim.keymap.set('n', '<tab>', api.node.open.vertical, opts('Open: Vertical Split'))
+        vim.keymap.set('n', '<c-p>', api.node.open.preview, opts('Open Preview'))
+      end,
       git = {
         ignore = false,
       },
