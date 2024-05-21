cfbbe8e847e35c46fe4039aebb94e20851c06ec9
Shun Asai <shun.asai@tmkc.co.jp>
2024-05-21 16:10:02 +0900

    Add toggleterm

--- nvim/.config/nvim-custom/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim-custom/lua/plugins/toggleterm.lua
@@ -4,17 +4,32 @@ return {
   keys = function(_, _)
     local Terminal = require("toggleterm.terminal").Terminal
 
+    -- Default terminal
+    local toggleterm_default = Terminal:new({
+      direction = "horizontal",
+      size = 30,
+      hidden = true,
+    })
+    function ToggleTermDefault(direction)
+      toggleterm_default.direction = direction
+      toggleterm_default:toggle()
+    end
+
     -- Lazygit
+    local toggleterm_lazygit_default = Terminal:new({
+      cmd = "lazygit",
+      direction = "float",
+      hidden = true,
+    })
     function ToggleTermLazygit()
-      Terminal:new({
-        cmd = "lazygit",
-        direction = "float",
-        hidden = true,
-      }):toggle()
+      toggleterm_lazygit_default:toggle()
     end
 
     return {
       { "<leader>g", "<cmd>lua ToggleTermLazygit()<cr>", desc = "Toggle Lazygit" },
+      -- { "<c-n>", "<cmd>lua ToggleTermDefault()<cr>", desc = "Toggle terminal", mode = { "n", "t" } },
+      { "<c-n>", function() ToggleTermDefault("float") end, desc = "Toggle terminal", mode = { "n", "t" } },
+      { "<c-k>", function() ToggleTermDefault("horizontal") end, desc = "Toggle terminal", mode = { "n", "t" } },
     }
   end,
   opts = {
