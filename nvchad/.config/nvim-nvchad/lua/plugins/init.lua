8c56670b4e99f94cdd3d7a6b97505d7ac53e1b0e
Shun Asai <shun.asai@tmkc.co.jp>
2024-05-23 17:59:36 +0900

    Update conform config

--- nvchad/.config/nvim-nvchad/lua/plugins/init.lua
+++ nvchad/.config/nvim-nvchad/lua/plugins/init.lua
@@ -5,8 +5,23 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
+    keys = {
+      { "<Leader>cf", function() require("conform").format { lsp_fallback = true } end, desc = "Format files" },
+    },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- css = { "prettier" },
+        -- html = { "prettier" },
+      },
+      -- format_on_save = {
+      --   -- These options will be passed to conform.format()
+      --   timeout_ms = 500,
+      --   lsp_fallback = true,
+      -- },
+    },
+    config = function(_, opts)
+      require("conform").setup(opts)
     end,
   },
 
