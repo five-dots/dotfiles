0aa5d8bb92f175b76a40ed3f4ec8048dc14d613e
Shun Asai <shun.asai@tmkc.co.jp>
2024-05-31 15:44:02 +0900

    Add shfmt formatter

--- nvchad/.config/nvim-nvchad/lua/configs/mason.lua
+++ nvchad/.config/nvim-nvchad/lua/configs/mason.lua
@@ -16,6 +16,7 @@ return {
     "sqlfluff",
     -- Default formatters
     "prettier",
+    "shfmt",
     "stylua",
     "yamlfmt",
     -- Additional formatters
--- nvchad/.config/nvim-nvchad/lua/plugins/conform.lua
+++ nvchad/.config/nvim-nvchad/lua/plugins/conform.lua
@@ -13,8 +13,14 @@ return {
     },
   },
   opts = {
+    formatters = {
+      shfmt = {
+        prepend_args = { "--indent", "4", "--binary-next-line" },
+      },
+    },
     formatters_by_ft = {
       lua = { "stylua" },
+      sh = { "shfmt" },
       yaml = { "yamlfmt" },
     },
     -- format_on_save = {
