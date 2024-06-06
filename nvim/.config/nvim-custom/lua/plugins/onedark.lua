06d067c97afccdb125a4edd4b73e4b79d14f8cc8
Shun Asai <shun.asai@tmkc.co.jp>
2024-06-06 14:44:57 +0900

    Update onedark theme config

--- nvim/.config/nvim-custom/lua/plugins/onedark.lua
+++ nvim/.config/nvim-custom/lua/plugins/onedark.lua
@@ -1,5 +1,19 @@
 return {
   "navarasu/onedark.nvim",
+  opts = {
+    style = "darker",
+    code_style = {
+      comments = "italic",
+      keywords = "italic",
+      functions = "italic,bold",
+      strings = "none",
+      variables = "none",
+    },
+    -- transparent = true,
+    -- lualine = {
+    --   transparent = true,
+    -- },
+  },
   config = function(_, opts)
     require("onedark").setup(opts)
     require("onedark").load()
