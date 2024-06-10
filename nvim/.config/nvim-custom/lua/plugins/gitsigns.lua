99a09a7c4b29c36a3989edbb67dcf91ec1ae290e
Shun Asai <shun.asai@tmkc.co.jp>
2024-06-10 15:39:28 +0900

    Update gitsigns config

--- nvim/.config/nvim-custom/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim-custom/lua/plugins/gitsigns.lua
@@ -1,4 +1,50 @@
 return {
   "lewis6991/gitsigns.nvim",
-  config = true,
+  opts = {
+    signs = {
+      add = { text = "│" },
+      change = { text = "│" },
+      delete = { text = "󰍵" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "│" },
+    },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function opts(desc)
+        return { buffer = bufnr, desc = desc }
+      end
+
+      local function next_hunk()
+        if vim.wo.diff then
+          vim.cmd.normal { "]h", bang = true }
+        else
+          gs.nav_hunk "next"
+        end
+      end
+      local function prev_hunk()
+        if vim.wo.diff then
+          vim.cmd.normal { "[h", bang = true }
+        else
+          gs.nav_hunk "prev"
+        end
+      end
+
+      local map = vim.keymap.set
+
+      map("n", "]h", next_hunk, opts "Next hunk")
+      map("n", "[h", prev_hunk, opts "Previous hunk")
+
+      map("n", "<Leader>gb", gs.blame_line, opts "Blame line")
+      map("n", "<Leader>gp", gs.preview_hunk, opts "Preview hunk")
+      map("n", "<Leader>gr", gs.reset_hunk, opts "Reset hunk")
+      map("n", "<Leader>gs", gs.stage_hunk, opts "Stage hunk")
+      map("n", "<Leader>gu", gs.undo_stage_hunk, opts "Undo stage hunk")
+      map("n", "<Leader>tb", gs.toggle_current_line_blame, opts "Blame line")
+    end,
+  },
+  config = function(_, opts)
+    require("gitsigns").setup(opts)
+  end,
 }
