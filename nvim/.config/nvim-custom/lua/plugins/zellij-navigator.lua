eb4ed5af2f4b16d40c33b5ed9504234c3966cf53
Shun Asai <shun.asai@tmkc.co.jp>
2024-06-10 09:58:23 +0900

    Add zellij navigator

---
+++ nvim/.config/nvim-custom/lua/plugins/zellij-navigator.lua
@@ -0,0 +1,15 @@
+return {
+  "hiasr/vim-zellij-navigator.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    {
+      "swaits/zellij-nav.nvim",
+      config = function()
+        require("zellij-nav").setup()
+      end
+    },
+  },
+  config = function()
+    require('vim-zellij-navigator').setup()
+  end,
+}
