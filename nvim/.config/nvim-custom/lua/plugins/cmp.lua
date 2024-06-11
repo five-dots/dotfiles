75dafec96e8e19a6022dfac4578db92cfe186e44
Shun Asai <shun.asai@tmkc.co.jp>
2024-06-11 14:15:46 +0900

    Update cmp config

--- nvim/.config/nvim-custom/lua/plugins/cmp.lua
+++ nvim/.config/nvim-custom/lua/plugins/cmp.lua
@@ -1,69 +1,172 @@
+local cmp = require "cmp"
+
 return {
   "hrsh7th/nvim-cmp",
   event = {
     "InsertEnter",
     "CmdlineEnter",
   },
+  opts = {
+    completion = {
+      completeopt = "menu,menuone,preview,noinsert,noselect",
+    },
+    view = {
+      docs = {
+        auto_open = false,
+      },
+    },
+    mapping = {
+      ["<Home>"] = cmp.mapping.abort(),
+      ["<PageUp>"] = cmp.mapping.scroll_docs(-4),
+      ["<PageDown>"] = cmp.mapping.scroll_docs(4),
+
+      -- Right for cmp and copilot
+      ["<Right>"] = cmp.mapping(function(fallback)
+        local suggestion = require "copilot.suggestion"
+        if cmp.get_active_entry() then
+          cmp.confirm { select = true, behavior = cmp.ConfirmBehavior.Replace }
+        elseif suggestion.is_visible() then
+          suggestion.accept()
+        else
+          fallback()
+        end
+      end, { "i", "s" }), -- i=insert, c=command, s=select
+
+      -- Down for cmp and copilot
+      ["<Down>"] = cmp.mapping(function(fallback)
+        local suggestion = require "copilot.suggestion"
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif suggestion.is_visible() then
+          suggestion.next()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      -- Up for cmp and copilot
+      ["<Up>"] = cmp.mapping(function(fallback)
+        local suggestion = require "copilot.suggestion"
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif suggestion.is_visible() then
+          suggestion.prev()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      -- Toggle completion
+      ["<C-Tab>"] = cmp.mapping(function()
+        if cmp.visible() then
+          cmp.close()
+        else
+          cmp.complete()
+        end
+      end, { "i", "s" }),
+
+      -- Toggle docs
+      ["<Insert>"] = cmp.mapping(function(fallback)
+        if not cmp.visible() then
+          fallback()
+        end
+        if cmp.visible_docs() then
+          cmp.close_docs()
+        else
+          cmp.open_docs()
+        end
+      end, { "i", "s" }),
+    },
+    sources = {
+      { name = "buffer" },
+      { name = "path" },
+      { name = "nvim_lsp" },
+      { name = "dictionary" },
+      -- { name = "nvim_lua" },
+      -- { name = "luasnip" },
+    },
+  },
+  config = function(_, opts)
+    cmp.setup(opts)
+  end,
   dependencies = {
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
+    -- "saadparwaiz1/cmp_luasnip",
     -- "L3MON4D3/LuaSnip",
     -- "hrsh7th/cmp-nvim-lua",
-    -- "saadparwaiz1/cmp_luasnip",
-  },
-  config = function()
-    local cmp = require "cmp"
-    cmp.setup {
-      mapping = {
-        ["<up>"] = cmp.mapping.select_prev_item(),
-        ["<down>"] = cmp.mapping.select_next_item(),
-        ["<c-s-tab>"] = cmp.mapping.complete(),
-        ["<home>"] = cmp.mapping.abort(),
-        ["<pageup>"] = cmp.mapping.scroll_docs(-4),
-        ["<pagedown>"] = cmp.mapping.scroll_docs(4),
-
-        -- Toggle docs
-        ["<insert>"] = cmp.mapping(
-          function(fallback)
-            if not cmp.visible() then
-              fallback()
-            end
-            if cmp.visible_docs() then
-              cmp.close_docs()
-            else
-              cmp.open_docs()
-            end
+    {
+      "hrsh7th/cmp-cmdline",
+      event = { "CmdLineEnter" },
+      opts = {
+        history = true,
+        updateevents = "CmdlineEnter,CmdlineChanged",
+      },
+      config = function()
+        -- Enable ":" completion
+        cmp.setup.cmdline(":", {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = "path" },
+          }, {
+            { name = "cmdline" },
+          }),
+          matching = { disallow_symbol_nonprefix_matching = false },
+        })
+        -- Enable "/" and "?" completion
+        cmp.setup.cmdline({ "/", "?" }, {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = {
+            { name = "buffer" },
+          },
+        })
+      end,
+    },
+    -- cmp-dictionary
+    {
+      "uga-rosa/cmp-dictionary",
+      config = function()
+        local dict_dir = os.getenv "XDG_CONFIG_HOME" .. "/nvim-cmp-dict/"
+        local dict = {
+          ["*"] = { dict_dir .. "all" },
+          ft = {
+            sql = { dict_dir .. "sql" },
+          },
+        }
+        require("cmp_dictionary").setup {
+          paths = dict["*"],
+        }
+        -- Enable dict by filetype
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = "*",
+          callback = function(ev)
+            local paths = dict.ft[ev.match] or {}
+            vim.list_extend(paths, dict["*"])
+            require("cmp_dictionary").setup {
+              paths = paths,
+            }
           end,
-          -- i=insert, c=command, s=select
-          { "i", "s" }
-        ),
-      },
-      sources = {
-        { name = "buffer" },
-        { name = "nvim_lsp" },
-        { name = "path" },
-        -- { name = "luasnip" },
-        -- { name = "nvim_lua" },
-      },
-    }
-
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
-      }),
-      matching = { disallow_symbol_nonprefix_matching = false },
-    })
-  end,
+        })
+      end,
+    },
+    -- copilot.lua
+    {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      config = function()
+        require("copilot").setup {
+          suggestion = {
+            auto_trigger = true,
+          },
+          panel = {
+            enabled = false,
+          },
+          filetypes = {
+            yaml = true,
+          },
+        }
+      end,
+    },
+  },
 }
