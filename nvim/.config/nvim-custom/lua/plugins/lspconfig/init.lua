2b155793004882a4ad81df7ece275b86df67ebbc
Shun Asai <shun.asai@tmkc.co.jp>
2024-06-13 12:39:13 +0900

    Update lspconfig

---
+++ nvim/.config/nvim-custom/lua/plugins/lspconfig/init.lua
@@ -0,0 +1,58 @@
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require "lspconfig"
+    local util = require "plugins.lspconfig.util"
+
+    local on_attach = util.on_attach
+    local capabilities = util.capabilities
+
+    -- Load server specific configs
+    require("plugins.lspconfig.bqls")
+    require("plugins.lspconfig.yamlls")
+
+    local servers = {
+      "lua_ls",
+      "bashls",
+      "bqls",
+      "gopls",
+      "jsonls",
+      "pyright",
+      "tsserver",
+      "yamlls",
+      "yamlls_dbt",
+    }
+
+    -- lsps with default config
+    for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
+    end
+
+    -- Disable underline for diagnostics
+    -- https://www.reddit.com/r/neovim/comments/lciqhp/disable_annoying_underline_when_make_errors/
+    vim.lsp.handlers["textDocument/publishDiagnostics"] =
+      vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { underline = false })
+  end,
+
+  dependencies = {
+    -- telescope.nvim
+    {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.6",
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    -- fidget.nvim
+    {
+      "j-hui/fidget.nvim",
+      opts = {},
+    },
+    -- neodev.nvim
+    {
+      "folke/neodev.nvim",
+      opts = {},
+    },
+  },
+}
---
+++ nvim/.config/nvim-custom/lua/plugins/lspconfig/util.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+local lsp_util = require "lspconfig.util"
+
+M.on_attach = function(_, bufnr)
+  local map = function(keys, func, desc)
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+  end
+  map("gd", require("telescope.builtin").lsp_definitions, "Go to definitions")
+  -- map("gd", vim.lsp.buf.definition, "Go to definition")
+  map("gD", vim.lsp.buf.declaration, "Go to declaration")
+  map("gI", require("telescope.builtin").lsp_implementations, "Go to implementations")
+  -- map("gI", vim.lsp.buf.implementation, "Go to implementation")
+  map("gr", require("telescope.builtin").lsp_references, "Go to references")
+  -- map("gr", vim.lsp.buf.references, "Go to references")
+  map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+  map("<Leader>ca", vim.lsp.buf.code_action, "Code Action")
+  map("<Leader>ch", vim.lsp.buf.signature_help, "Signature help")
+  map("<Leader>ci", "<Cmd>LspInfo<CR>", "LSP info")
+  map("<Leader>cl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, "List workspace folders")
+  map("<Leader>cr", vim.lsp.buf.rename, "Rename")
+  map("<Leader>cs", require("telescope.builtin").lsp_document_symbols, "Document symbols")
+  map("<Leader>cS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace symbols")
+  map("<Leader>ct", require("telescope.builtin").lsp_type_definitions, "Type definitions")
+  -- map("<Leader>ct", vim.lsp.buf.type_definition, "Type definition")
+  map("<Leader>cw", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
+  map("<Leader>cW", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
+end
+
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+
+-- Define root_dir function that ignores dbt projects
+M.find_git_ancestor_ignore_dbt = function(fname)
+  local root_dir = lsp_util.find_git_ancestor(fname)
+  local is_dbt_project = lsp_util.path.is_file(lsp_util.path.join(root_dir, "dbt_project.yml"))
+  if is_dbt_project then
+    return nil
+  end
+  return root_dir
+end
+
+return M
