27f003e9977a3b31d561c5a60ab9381da2dba818
Shun Asai <shun.asai@tmkc.co.jp>
2024-07-29 17:56:24 +0900

    Update tmux version

---
+++ nvim/.config/nvim-custom/lua/plugins/tmux.lua
@@ -0,0 +1,15 @@
+return {
+  "aserowy/tmux.nvim",
+  config = function()
+    return require("tmux").setup{
+      navigation = {
+        enable_default_keybindings = false,
+      },
+      resize = {
+        enable_default_keybindings = false,
+        resize_step_x = 1,
+        resize_step_y = 1,
+      }
+    }
+  end,
+}
--- nvim/.config/nvim-custom/lua/plugins/zellij-navigator.lua
+++ nvim/.config/nvim-custom/lua/plugins/zellij-navigator.lua
@@ -1,11 +1,18 @@
 return {
   "hiasr/vim-zellij-navigator.nvim",
+  enabled = false,
   event = "VeryLazy",
   dependencies = {
     {
       "swaits/zellij-nav.nvim",
       config = function()
         require("zellij-nav").setup()
+      end,
+    },
+    {
+      "dynamotn/Navigator.nvim",
+      config = function()
+        require("Navigator").setup()
       end,
     },
   },
