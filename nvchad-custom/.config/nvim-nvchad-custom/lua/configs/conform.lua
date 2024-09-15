1b49edbc074eaac44b3336c771715a0d9951a134
Shun Asai <shun.asai@tmkc.co.jp>
2024-09-15 13:58:35 +0900

    Add conform and trouble

--- nvchad-custom/.config/nvim-nvchad-custom/lua/configs/conform.lua
+++ nvchad-custom/.config/nvim-nvchad-custom/lua/configs/conform.lua
@@ -1,15 +1,24 @@
-local options = {
+local util = require("conform.util")
+
+return {
+  formatters = {
+    shfmt = {
+      prepend_args = { "--indent", "4", "--binary-next-line" },
+    },
+    sqlfluff = {
+      command = util.find_executable({ ".venv/bin/sqlfluff" }, "sqlfluff"),
+    },
+  },
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    python = { "black" },
+    sh = { "shfmt" },
+    -- sql = { "sqlfluff", "sqlfmt" }, -- Use LSP fallback
+    yaml = { "yamlfmt" },
   },
-
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
   --   timeout_ms = 500,
   --   lsp_fallback = true,
   -- },
 }
-
-return options
